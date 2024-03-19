84982825fe8c0e81b8252e23f46a3a7faebfc8d0
antoniop <antoniop.silv@gmail.com>
2024-03-18 22:44:57 -0300

    feat: Delivery, Service running in different process

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementService.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementService.kt
@@ -16,11 +16,12 @@ class IncrementService : Service() {
         override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
             msg.data.getInt("VALUE").also {
-                InterEntityCommunication.valueLiveData.postValue(it + 1)
+                Intent("INCREMENT_VALUE_ACTION").putExtra("VALUE", it + 1).apply {
+                    sendBroadcast(this)
+                }
             }
             stopSelf()
         }
-
     }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.getIntExtra("VALUE", -1)?.also { value ->
